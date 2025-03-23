9e2ff1ed2fc6b1f8176a7fc203cb6d2e2be6d53c
Aditya Shrestha <101134513+Adityastha@users.noreply.github.com>
2025-03-24 00:18:02 +0545

    Add menu n list with images and category names

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,53 @@
 import logo from "./logo.png";
 import search_icon from "./search-icon.png";
 import take_away from "./take-away.png";
+import menu_1 from "./menu_1.png";
+import menu_2 from "./menu_2.png";
+import menu_3 from "./menu_3.png";
+import menu_4 from "./menu_4.png";
+import menu_5 from "./menu_5.png";
+import menu_6 from "./menu_6.png";
+import menu_7 from "./menu_7.png";
+import menu_8 from "./menu_8.png";
+
 export const assets = {
   logo,
   search_icon,
   take_away,
 };
-export const url = "http://localhost:5173";
+
+export const menu_list = [
+  {
+    menu_name: "Salad",
+    menu_image: menu_1,
+  },
+  {
+    menu_name: "Rolls",
+    menu_image: menu_2,
+  },
+  {
+    menu_name: "Deserts",
+    menu_image: menu_3,
+  },
+  {
+    menu_name: "Sandwich",
+    menu_image: menu_4,
+  },
+  {
+    menu_name: "Cake",
+    menu_image: menu_5,
+  },
+  {
+    menu_name: "Pure Veg",
+    menu_image: menu_6,
+  },
+  {
+    menu_name: "Pasta",
+    menu_image: menu_7,
+  },
+  {
+    menu_name: "Noodles",
+    menu_image: menu_8,
+  },
+];
+// export const url = "http://localhost:5173";
