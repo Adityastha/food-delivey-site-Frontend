67a1dda9fb3a800298e88d4a309d1b3849199849
Aditya Shrestha <101134513+Adityastha@users.noreply.github.com>
2025-03-25 01:07:09 +0545

    feat: Add OptionsMenu component for Menu category selection

---
+++ src/components/OptionsMenu/OptionsMenu.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./OptionsMenu.css";
+import { categories } from "../../assets/assets";
+const OptionsMenu = ({ category, setCategory }) => {
+  return (
+    <div>
+      <div className="options-menu">
+        <h1>What would you like to eat?</h1>
+        <p className="options-menu-text">
+          Craving something delicious? Explore our menu and find your perfect
+          meal!
+        </p>
+        <div className="options-menu-list">
+          {categories.map((item, index) => {
+            return (
+              <div
+                onClick={() =>
+                  setCategory((prev) =>
+                    prev === item.name ? "all" : item.name
+                  )
+                }
+                key={index}
+                className="options-menu-list-item"
+              >
+                <img
+                  className={category === item.name ? "active" : ""}
+                  src={item.image}
+                  alt=""
+                />
+                <p>{item.name}</p>
+              </div>
+            );
+          })}
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default OptionsMenu;
