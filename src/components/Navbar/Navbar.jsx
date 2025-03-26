394fe9f4e968c506bb9c8f1c2f67560d347991f6
Aditya Shrestha <101134513+Adityastha@users.noreply.github.com>
2025-03-26 19:08:44 +0545

    Added navigation links and sign-in button to the Navbar for improved user access.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,45 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("contact");
   return (
     <div className="navbar">
       <div className="logo">
-        <img src={assets.logo} alt="food delivery site logo" />
+        <a href="#home">
+          {" "}
+          <img src={assets.logo} alt="food delivery site logo" />
+        </a>
       </div>
       <ul className="navbar-menu">
-        <li
+        <a
+          href="#home"
           onClick={() => setMenu("home")}
           className={menu === "home" ? "active" : ""}
         >
           Home
-        </li>
-        <li
+        </a>
+        <a
+          href="#options-menu"
           onClick={() => setMenu("menu")}
           className={menu === "menu" ? "active" : ""}
         >
           Menu
-        </li>
-        <li
+        </a>
+        <a
+          href="#"
           onClick={() => setMenu("cart")}
           className={menu === "cart" ? "active" : ""}
         >
           Cart
-        </li>
-        <li
+        </a>
+        <a
+          href="#footer"
           onClick={() => setMenu("contact")}
           className={menu === "contact" ? "active" : ""}
         >
           Contact
-        </li>
+        </a>
       </ul>
       <div className="navbar-right">
         <img
@@ -44,7 +51,7 @@ const Navbar = () => {
           <img src={assets.take_away} alt="" className="take-away" />
           <div className="cart-notification"></div>
         </div>
-        <button>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   );
